84d2f98be0614c5511efa265f423ace7eeb9f2a4
Caio Dantas <caio.dantas@elo7.com>
2021-11-22 11:55:45 -0300

    Implementa ajuste de ícone na tela de descrição

--- app/src/main/java/com/example/apppdi/ui/activities/ActivityDetailsRepo.kt
+++ app/src/main/java/com/example/apppdi/ui/activities/ActivityDetailsRepo.kt
@@ -1,8 +1,10 @@
 package com.example.apppdi.ui.activities
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.webkit.WebView
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.apppdi.R
@@ -21,12 +23,18 @@ class ActivityDetailsRepo : AppCompatActivity() {
         val txtDescDetail = findViewById<TextView>(R.id.txtDescDetail)
         val txtStars = findViewById<TextView>(R.id.txtStars)
         val txtForks = findViewById<TextView>(R.id.txtForks)
+        val image = findViewById<ImageView>(R.id.imgVisibilityDesc)
 
         webviewReadme.loadUrl(repo.html_url_readme.toString())
         txtFullname.text = repo.full_name
         txtDescDetail.text = repo.description
         txtStars.text = repo.watchers_count.toString()
         txtForks.text = repo.forks_count.toString()
+
+        val visibilityRes = if (repo.private) android.R.drawable.ic_secure else android.R.drawable.ic_partial_secure
+        val visibilityColor = if (repo.private) Color.BLACK else Color.LTGRAY
+        image.setImageResource(visibilityRes)
+        image.setColorFilter(visibilityColor)
 
     }
 }
